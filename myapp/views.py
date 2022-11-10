56de9f1d22d0d05e44c38a6eefc69612cd646494
Mahadev_Gaikwad <gaikwaddev01@gmail.com>
2022-11-10 17:13:15 +0530

    Image added

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,5 @@ from .models import Job
 def home(request):
     if request.method == 'GET':
         data = Job.objects.all()
-
-    # return HttpResponse("Hi this is my Home Page")
         return render(request,'myapp/home.html',{'data':data})
+    
